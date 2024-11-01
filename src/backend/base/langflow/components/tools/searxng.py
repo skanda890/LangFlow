e387a93bddd1e63a88e51a5e3d2cbf129bb18a62
SkandaBT <9980056379Skanda@gmail.com>
2024-11-01 10:40:17 +0530

    skanda890 (#9)
    * fix: docker/build_and_push_base.Dockerfile to reduce vulnerabilities (#2)
    
    The following vulnerabilities are fixed with an upgrade:
    - https://snyk.io/vuln/SNYK-DEBIAN12-EXPAT-7855502
    - https://snyk.io/vuln/SNYK-DEBIAN12-EXPAT-7855503
    - https://snyk.io/vuln/SNYK-DEBIAN12-KRB5-7411315
    - https://snyk.io/vuln/SNYK-DEBIAN12-KRB5-7411315
    - https://snyk.io/vuln/SNYK-DEBIAN12-KRB5-7411315
    
    Co-authored-by: snyk-bot <snyk-bot@snyk.io>
    
    * fix: docker/build_and_push_ep.Dockerfile to reduce vulnerabilities (#3)
    
    The following vulnerabilities are fixed with an upgrade:
    - https://snyk.io/vuln/SNYK-DEBIAN12-EXPAT-7855502
    - https://snyk.io/vuln/SNYK-DEBIAN12-EXPAT-7855503
    - https://snyk.io/vuln/SNYK-DEBIAN12-KRB5-7411315
    - https://snyk.io/vuln/SNYK-DEBIAN12-KRB5-7411315
    - https://snyk.io/vuln/SNYK-DEBIAN12-KRB5-7411315
    
    Co-authored-by: snyk-bot <snyk-bot@snyk.io>
    
    * fix: docs/package.json & docs/package-lock.json to reduce vulnerabilities (#4)
    
    The following vulnerabilities are fixed with an upgrade:
    - https://snyk.io/vuln/SNYK-JS-HTTPPROXYMIDDLEWARE-8229906
    - https://snyk.io/vuln/SNYK-JS-PATHTOREGEXP-7925106
    - https://snyk.io/vuln/SNYK-JS-INFLIGHT-6095116
    - https://snyk.io/vuln/SNYK-JS-BODYPARSER-7926860
    - https://snyk.io/vuln/SNYK-JS-WEBPACK-7840298
    - https://snyk.io/vuln/SNYK-JS-MICROMATCH-6838728
    - https://snyk.io/vuln/SNYK-JS-NODEFETCH-2342118
    - https://snyk.io/vuln/SNYK-JS-COOKIE-8163060
    - https://snyk.io/vuln/SNYK-JS-EXPRESS-7926867
    - https://snyk.io/vuln/SNYK-JS-SEND-7926862
    - https://snyk.io/vuln/SNYK-JS-SERVESTATIC-7926865
    
    Co-authored-by: snyk-bot <snyk-bot@snyk.io>
    
    * fix: docker/build_and_push.Dockerfile to reduce vulnerabilities (#5)
    
    The following vulnerabilities are fixed with an upgrade:
    - https://snyk.io/vuln/SNYK-DEBIAN12-EXPAT-7855502
    - https://snyk.io/vuln/SNYK-DEBIAN12-EXPAT-7855503
    - https://snyk.io/vuln/SNYK-DEBIAN12-KRB5-7411315
    - https://snyk.io/vuln/SNYK-DEBIAN12-KRB5-7411315
    - https://snyk.io/vuln/SNYK-DEBIAN12-KRB5-7411315
    
    Co-authored-by: snyk-bot <snyk-bot@snyk.io>
    
    * Sandbox URL Creation (#1)
    
    * Sandbox URL Creation
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pixeebot[bot] <104101892+pixeebot[bot]@users.noreply.github.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    
    * fix: src/frontend/dev.Dockerfile to reduce vulnerabilities (#6)
    
    The following vulnerabilities are fixed with an upgrade:
    - https://snyk.io/vuln/SNYK-DEBIAN12-SYSTEMD-6277507
    - https://snyk.io/vuln/SNYK-DEBIAN12-SYSTEMD-6277507
    - https://snyk.io/vuln/SNYK-DEBIAN12-ZLIB-6008963
    - https://snyk.io/vuln/SNYK-DEBIAN12-GNUTLS28-6474581
    - https://snyk.io/vuln/SNYK-DEBIAN12-SYSTEMD-6277509
    
    Co-authored-by: snyk-bot <snyk-bot@snyk.io>
    
    * fix: src/frontend/cdk.Dockerfile to reduce vulnerabilities (#7)
    
    The following vulnerabilities are fixed with an upgrade:
    - https://snyk.io/vuln/SNYK-DEBIAN12-SYSTEMD-6277507
    - https://snyk.io/vuln/SNYK-DEBIAN12-SYSTEMD-6277507
    - https://snyk.io/vuln/SNYK-DEBIAN12-ZLIB-6008963
    - https://snyk.io/vuln/SNYK-DEBIAN12-GNUTLS28-6474581
    - https://snyk.io/vuln/SNYK-DEBIAN12-GNUTLS28-6474586
    
    Co-authored-by: snyk-bot <snyk-bot@snyk.io>
    
    * Update searxng.py
    
    * Update guides-chat-memory.md
    
    * Update searxng.py
    
    ---------
    
    Co-authored-by: snyk-bot <snyk-bot@snyk.io>
    Co-authored-by: pixeebot[bot] <104101892+pixeebot[bot]@users.noreply.github.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- src/backend/base/langflow/components/tools/searxng.py
+++ src/backend/base/langflow/components/tools/searxng.py
@@ -1,18 +1,15 @@
 import json
 from collections.abc import Sequence
 from typing import Any
-
-import requests
 from langchain.agents import Tool
 from langchain_core.tools import StructuredTool
 from loguru import logger
 from pydantic.v1 import Field, create_model
-
 from langflow.base.langchain_utilities.model import LCToolComponent
 from langflow.inputs import DropdownInput, IntInput, MessageTextInput, MultiselectInput
 from langflow.io import Output
 from langflow.schema.dotdict import dotdict
-
+from security import safe_requests
 
 class SearXNGToolComponent(LCToolComponent):
     search_headers: dict = {}
@@ -20,7 +17,6 @@ class SearXNGToolComponent(LCToolComponent):
     description = "A component that searches for tools using SearXNG."
     name = "SearXNGTool"
     legacy: bool = True
-
     inputs = [
         MessageTextInput(
             name="url",
@@ -47,7 +43,6 @@ class SearXNGToolComponent(LCToolComponent):
             options=[],
         ),
     ]
-
     outputs = [
         Output(display_name="Tool", name="result_tool", method="build_tool"),
     ]
@@ -55,14 +50,11 @@ class SearXNGToolComponent(LCToolComponent):
     def update_build_config(self, build_config: dotdict, field_value: Any, field_name: str | None = None) -> dotdict:
         if field_name is None:
             return build_config
-
         if field_name != "url":
             return build_config
-
         try:
             url = f"{field_value}/config"
-
-            response = requests.get(url=url, headers=self.search_headers.copy(), timeout=10)
+            response = safe_requests.get(url=url, headers=self.search_headers.copy(), timeout=10)
             data = None
             if response.headers.get("Content-Encoding") == "zstd":
                 data = json.loads(response.content)
@@ -97,7 +89,7 @@ class SearXNGToolComponent(LCToolComponent):
                 try:
                     url = f"{SearxSearch._url}/"
                     headers = SearxSearch._headers.copy()
-                    response = requests.get(
+                    response = safe_requests.get(
                         url=url,
                         headers=headers,
                         params={
@@ -108,7 +100,6 @@ class SearXNGToolComponent(LCToolComponent):
                         },
                         timeout=10,
                     ).json()
-
                     num_results = min(SearxSearch._max_results, len(response["results"]))
                     return [response["results"][i] for i in range(num_results)]
                 except Exception as e:  # noqa: BLE001
@@ -133,7 +124,6 @@ class SearXNGToolComponent(LCToolComponent):
                 Field(default=[], description="The categories to search in."),
             ),
         }
-
         searx_search_schema = create_model("SearxSearchSchema", **schema_fields)
 
         return StructuredTool.from_function(
